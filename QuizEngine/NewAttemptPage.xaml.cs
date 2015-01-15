6ab389bc704fbf49876fa94d72be1b82ef7b610c
Patrick Lea <ledpup@gmail.com>
2015-01-15 13:23:10 +1100

    New anatomy questions

--- QuizEngine/NewAttemptPage.xaml.cs
+++ QuizEngine/NewAttemptPage.xaml.cs
@@ -122,16 +122,10 @@ namespace QuizEngine
         private void StartQuiz_Click(object sender, RoutedEventArgs e)
         {
             _quizConfig.NumberOfQuestions = (int)NumberOfQuestions.Value;
-            _quizQuestions.Shuffle();
-            //_quizQuestions.Reverse();
-            //if ((bool) RandomOrder.IsChecked)
-            //{
-            //    _quizQuestions.Shuffle();
-            //}
-            //else if ((bool) NewestToOldest.IsChecked)
-            //{
-            //    _quizQuestions.Reverse();
-            //}
+
+            //_quizQuestions.Shuffle();
+            _quizQuestions.Reverse();
+
             _quizQuestions = _quizQuestions.Take(_quizConfig.NumberOfQuestions).ToList();
 
             for (var i = 0; i < _quizQuestions.Count; i++)
